45ae3c4f7b9b38308586d3fe0159a1345b83ab11
Guo, Jingfeng <jingfeng.guo@intel.com>
2020-09-25 11:10:15 +0800

    fix bug

--- decode.go
+++ decode.go
@@ -1,6 +1,6 @@
 package gcToWg
 
-func to_gps (lat, lng float64) (float64, float64) {
+func To_gps (lat, lng float64) (float64, float64) {
 	d_lat, d_lng := delta(lat, lng)
 	return lat - d_lat, lng -d_lng
 }
